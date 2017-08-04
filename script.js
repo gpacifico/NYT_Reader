351fce77f35ef9a3044de4ce1817dd512aab4e3a
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-08-04 13:36:15 -0700

    changed head and made api call function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Let's use the News Headline API and Text to Speech API I sent you.
+
+function getNYT() {
+    $.ajax({
+        dataType: 'json',
+        type: 'get',
+        data: {
+            api_key: 'comingsoon'
+        },
+        url: url.get,
+        success: function(result) {
+            console.log("it's reading");
+        }
+    });
+}
