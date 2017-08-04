58d2e3546c1d009d71c88eb17368d857733daeec
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-08-04 14:27:27 -0700

    added US english voice, startcallback, endcallback

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getNYT() {
         },
         url: "https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=3788190a07ca43fdaf2083bef6b256d2",
         success: function(story) {
-            console.log("it's reading");
+            console.log(story.articles);
         }
     });
 }
